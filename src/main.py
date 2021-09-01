e8dde06508fcc942a80e73221e47c2232e400719
flex <omareltayeb529@gmail.com>
2021-09-01 15:47:00 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,112 +1,169 @@
 import json
-def read_contacts():
-	with open("contacts.json", "r") as f:
-			contacts = json.loads(f.read())
+import jsonpickle
 
-			return contacts;
+
+
+def read_contacts():
+    with open("contacts.json", "r") as f:
+            contacts = json.loads(f.read())
+
+            return contacts;
+
+
+
+
+def get_contact_info(contact, specific=False):
+    if not specific:
+
+        info = contact[1][0]
+    else:
+        info = contact
+    number = info["number"]
+    email = info["email"]
+    note = info["additional info"]
+
+    return [number, email, note]
+
+
+
 def write_contacts():
-	samplejson = {"SampleContact": [{"number": "7148729143"}]}
-	with open("contacts.json", "w") as f:
-		json.dump(samplejson, f, indent=4)
+    samplejson = {"SampleContact": [{"number": "7148729143", "email": "email@email.com", "additional info": "note"}]}
+    with open("contacts.json", "w") as f:
+        json.dump(samplejson, f, indent=4)
+
+
+
 
 def open_contacts():
-	try:
+    try:
 
-		contacts = read_contacts()
-	except FileNotFoundError:
-		samplejson = {"SampleContact": [{"number": "7148729143"}]}
-		write_contacts()
-	
+        contacts = read_contacts()
+    except FileNotFoundError:
+        write_contacts()
+    
 
-	return read_contacts();
+    return read_contacts();
 
-def add_contact(name, number, filename='contacts.json'):
-	with open(filename,'r+') as file:
 
-		file_data = json.load(file)
 
-		file_data[name] = [{
-	"number": number,
+
+def add_contact(name, number, email, note):
+    with open("contacts.json",'r') as file:
+
+        file_data = json.load(file)
+
+        file_data[name] = [{
+    "number": number,
+    "email": email,
+    "additional info": note
 }]
-		file.seek(0)
+        file.seek(0)
 
-		json.dump(file_data, file, indent = 4)
+    with open("contacts.json","w") as file:
+        json.dump(file_data, file, indent = 4)
 
 
-	return f"Done adding contact {name} with number {number}"
+    return f"Done adding/updating contact {name}\n"
+
+
 
 def delete_contact(name, filename="contacts.json"):
-	with open(filename, "r") as file:
+    with open(filename, "r") as file:
 
-		data = json.load(file)
-		try:
+        data = json.load(file)
+        try:
 
-			data.pop(name)
-		except KeyError:
-			return f"No contact with the name {contact} exists." 
+            data.pop(name)
+        except KeyError:
+            return f"No contact with the name {contact} exists.\n" 
 
-	with open(filename, "w") as file:
-		json.dump(data, file, indent=4)
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
 
-	return f"Done deleting contact {name}"
-
-def showall(contacts):
-	output = ""
-
-	for i in contacts.items():
-		name = i[0]
-		info = i[1][0]
-		number = info["number"]
-
-		output += (f"\n{name}:( \n	number: {number}\n)")
-
-	return output;
-
-def show(contacts, contact):
-	output = ""
-	try:
-		info = contacts[contact][0]
-
-	except KeyError:
-		return f"No contact with the name {contact} exists."
-
-	number = info["number"]
+    return f"Done deleting contact {name}\n"
 
 
-	output = (f"\n{contact}: \n	number: {number}\n")
 
-	return output;
+
+def show(contacts, contact=None):
+    output = ""
+    index = 0
+    if contact == None:
+        for contact in contacts:
+            for contactinfo in contacts.items():
+                name = contact
+                listofinfo = get_contact_info(contactinfo)
+                if name not in output:
+
+                    output += (f"\n{name}:( \n  number: {listofinfo[0]}\n  email: {listofinfo[1]}\n  additional info: {listofinfo[2]}\n)\n\n")
+    else:
+        try:
+            info = contacts[contact][0]
+        except KeyError:
+            return "no contact with the name {contact} exists."
+        name = contact
+        listofinfo = get_contact_info(info, True)
+        output = (f"\n{name}:( \n  number: {listofinfo[0]}\n  email: {listofinfo[1]}\n  additional info: {listofinfo[2]}\n)\n\n")
+
+
+    return output;
+
+
+
+
+def insert(choice):
+    number, email, note = "unknown", "unknown", "unknown"
+    listofinfo = choice.split(" ")
+    listofinfo = [x for x in listofinfo if x != ""]
+    try:
+        name = listofinfo[1]
+    except IndexError:
+        return False
+    try:
+        number = listofinfo[2]
+        email = listofinfo[3]
+        note = listofinfo[4]
+    except IndexError:
+        pass
+
+    return name, number, email, note
+
+
+
 
 def main():
-	contacts = open_contacts()
-	print("\ntype in 'help' for info\n")
-	while True:
-		choice = input()
-		if choice in ["showall", "showall "]:
-			print(showall(contacts))
+    contacts = open_contacts()
+    print("\ntype in 'help' for info\n")
+    while True:
+        choice = input()
+        if choice in ["showall", "showall "]:
+            print(show(contacts))
 
-		elif "show" in choice and choice.split(" ")[0] == "show":
-			contact = choice[5:]
-			print(show(contacts, contact))
+        elif "show" in choice and choice.split(" ")[0] == "show":
+            contact = choice[5:]
+            print(show(contacts, contact))
 
-		elif "insert" in choice:
-			listofinfo = choice.split(" ")
-			name = listofinfo[1]
-			number = listofinfo[2]
-			print(add_contact(name, number))
+        elif "insert" in choice:
+            try:
 
-		elif "remove" in choice:
-			listofinfo = choice.split(" ")
-			name = "".join([listofinfo[1]])
-			print(delete_contact(name))
+                name, number, email, note = insert(choice)
+                print(add_contact(name, number, email, note))
+            except TypeError:
+                print("insert requires an argument")
+                continue
+            
+        elif "remove" in choice:
+            listofinfo = choice.split(" ")
+            name = "".join([listofinfo[1]])
+            print(delete_contact(name))
 
-		elif choice == "help":
-			print("showall: shows all contacts\nshow (contactname): shows a specific contact\ninsert (contactname) (number): creates a new contact\nremove (contactname): removes the specified contact")
+        elif choice == "help":
+            print("\nshowall: shows all contacts\nshow (contactname): shows a specific contact\ninsert (contactname), optional(number), optional(email), optional(note): creates a new contact or updates an existing one\nremove (contactname): removes the specified contact\n\n")
 
-		else:
-			print("Invalid command.")
+        else:
+            print("Invalid command.\n\n")
 
-		contacts = open_contacts()
+        contacts = open_contacts()
 
 
 
