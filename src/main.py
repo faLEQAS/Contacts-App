64e3c9137a28258ef4a705eec80b72b845a05997
flex <omareltayeb529@gmail.com>
2021-09-03 21:52:39 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 import json
 
 
@@ -70,10 +71,14 @@ def delete_contact(name, filename="contacts.json"):
 
     return True;
 
+
+
 def dict_to_individual_vars(info): #another best name function
     name, number, email, note = info["name"], info["number"], info["email"], info["note"]
 
     return name, number, email, note
+
+
 
 def prettyprintinfo(dictofinfo):
     name, number, email, note = dictofinfo["name"], dictofinfo["number"], dictofinfo["email"], dictofinfo["note"]
@@ -81,17 +86,20 @@ def prettyprintinfo(dictofinfo):
 
     return output;
 
+
+
 def show(contacts, contact=None):
     try:
         info = contacts[contact][0]
     except KeyError:
         return False
-
     name, number, email, note = contact, info["number"], info["email"], info["additional info"]
 
     dictofinfo = {"name": name, "number": number, "email": email, "note": note}
 
     return prettyprintinfo(dictofinfo)
+
+
 
 def showall(contacts):
     output = """"""
@@ -101,14 +109,15 @@ def showall(contacts):
     return output;
 
 
+
+
 def extract_info_from_user_input(choice): #best function name
     number, email, note = "unknown", "unknown", "unknown"
     listofinfo = choice.split(" ")
     listofinfo = [x for x in listofinfo if x != ""]
-    try:
-        name = listofinfo[1]
-    except IndexError:
+    if len(listofinfo) > 5 or len(listofinfo) == 1:
         return False
+    name = listofinfo[1]
     try:
         number = listofinfo[2]
         email = listofinfo[3]
@@ -120,44 +129,76 @@ def extract_info_from_user_input(choice): #best function name
 
 
 
+def print_help():
+    print("\nshowall: shows all contacts\nshow (contactname): shows a specific contact\ninsert (contactname), optional(number), optional(email), optional(note): creates a new contact or updates an existing one\nremove (contactname): removes the specified contact\n\n")
+
+
+
+def check_command(choice, contacts):
+    choicelist = choice.split(" ")
+    match choicelist[0]:
+        case "insert":
+            Command.Insert(choice)
+        case "remove":
+            Command.remove(choice)
+        case "show":
+            Command.show(choice, contacts)
+        case "help":
+            Command.help()
+        case "showall":
+            Command.showallcmd(contacts)
+        case _:
+            print("\nInvalid Command.")
 
 def main():
     print("\ntype in 'help' for info\n")
     while True:
         contacts = open_contacts()
         choice = input()
-        if choice in ["showall", "showall "]:
-            print(showall(contacts))
+        check_command(choice, contacts)
 
-        elif "show" in choice and choice.split(" ")[0] == "show":
-            contact = choice[5:]
-            print(show(contacts, contact))
 
-        elif "insert" in choice:
-            try:
-                info = extract_info_from_user_input(choice)
-                name, number, email, note = dict_to_individual_vars(info)
-                print(add_contact(name, number, email, note))
-            except TypeError:
-                print("insert requires an argument")
-                continue
-            
-        elif "remove" in choice:
-            listofinfo = choice.split(" ")
+class Command:
+    def showallcmd(contacts):
+        print(showall(contacts))
+    def help():
+        print("\nshowall: shows all contacts\nshow (contactname): shows a specific contact\ninsert (contactname), optional(number), optional(email), optional(note): creates a new contact or updates an existing one\nremove (contactname): removes the specified contact\n\n")
+    def Insert(choice):
+        info = extract_info_from_user_input(choice)
+        if info != False:
+
+            name, number, email, note = dict_to_individual_vars(info)
+            print(add_contact(name, number, email, note))
+        else:
+            print("Insert takes from 1 to 4 arguments\n")
+
+    def show(choice, contacts):
+        try:
+            choicelist = choice.split(" ")
+            print(choicelist)
+            contact = choicelist[1]
+            worked = show(contacts, contact)
+            if worked == False:
+                print(f"No contact with the name {contact} exists.")
+            else:
+                print(worked)
+        except IndexError:
+            print("show requires an argument")
+
+    def remove(choice):
+        listofinfo = choice.split(" ")
+        try:
+
             name = "".join([listofinfo[1]])
             worked = delete_contact(name)
             if worked == True:
                 print(f"Done deleting contact {name}\n")
             else:
                 print(f"No contact called {name} exists")
+        except IndexError:
+            print("remove requires an argument\n")
 
-        elif choice == "help":
-            print("\nshowall: shows all contacts\nshow (contactname): shows a specific contact\ninsert (contactname), optional(number), optional(email), optional(note): creates a new contact or updates an existing one\nremove (contactname): removes the specified contact\n\n")
-
-        elif choice not in [" ", ""]:
-            print("Invalid command.\n\n")
-
-
+        
 
 
 main()
